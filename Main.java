e0068f8d88e105c3045f157733a3f4dfa768f031
TEJESH PASALA <123167938+TEJESH2004@users.noreply.github.com>
2024-12-06 23:34:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+ public class Main{
+ public static void main (String[]args){
+    /* //System.out.print("HEllo tejesh");
+    System.out.println(300); */
+    System.out.println(500);
+    char teju = 'T';
+    System.out.println(teju);
+    int num=34;
+    float hey=2.373f;
+    String k = "Tejesh";
+    boolean haha= true;
+    if(haha = true){
+        System.out.println("\"DHoole maya\"");
+    }
+    int j , z , g ;
+    j=23;
+    g=23;
+    g=j;
+    System.out.println(Math.random());
+
+    System.out.println(j==g);
+ }
+    
+}
